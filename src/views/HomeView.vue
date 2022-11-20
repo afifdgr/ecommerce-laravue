ac16ee20be087604ee653cff450933b7232fef5f
afifdgr <afifdgr3@gmail.com>
2022-11-20 13:32:13 +0700

    # Convert HTML Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+
+    <Hero />
+
+    <Banner />
+
+    <Instagram />
+
+    <Partner />
+
+    <Footer />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Banner from '@/components/Banner.vue'
+import Instagram from '@/components/Instagram.vue'
+import Partner from '@/components/Partner.vue'
+import Footer from '@/components/Footer.vue'
+
 
 export default {
-  name: 'HomeView',
+  name: 'home',
   components: {
-    HelloWorld
+    Header,
+    Hero,
+    Banner,
+    Instagram,
+    Partner,
+    Footer
   }
 }
 </script>
